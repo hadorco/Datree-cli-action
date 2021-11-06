0367f86d1a532858dbd8e6a2d01024cd9440e057
hadorco <54988869+hadorco@users.noreply.github.com>
2021-11-06 20:54:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello"
-time=$(date)
-echo "::set-output name=time::$time"
+options="$INPUT_OPTIONS"
+
 curl https://get.datree.io | /bin/bash
-datree test /test.yaml
+datree test /test.yaml $options
